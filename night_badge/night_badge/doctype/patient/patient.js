acd484b7d71f786394f9a309212ec39b6f4770ca
Faisal Zahoor <faisal-zahoor>
2023-10-16 09:52:29 +0500

    update

--- night_badge/night_badge/doctype/patient/patient.js
+++ night_badge/night_badge/doctype/patient/patient.js
@@ -2,7 +2,39 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Patient', {
-	// refresh: function(frm) {
+	refresh(frm) {
+		frm.add_custom_button('Dead', () => {
+			frm.call("dead")	
+		});
+	},
+	cnic_no: function(frm) {
+		console.log('Lol 🎉 ');
+	},
+});
 
-	// }
+frappe.ui.form.on('Visits', {
+	before_visits_remove(frm, cdt, cdn) {
+		let date = locals[cdt][cdn].date
+		console.log(`A row is about to removed 🎉 ${date}`);
+	},
+	
+	visits_remove(frm, cdt, cdn) {
+		console.log(`A row is removed 🎉`);
+	},
+
+	visits_add(frm, cdt, cdn) {
+		console.log(`A row is added 🎉`);
+	},
+
+	visits_move(frm, cdt, cdn) {
+		console.log(`A row is moved 🎉`);
+	},
+
+	form_render(frm, cdt, cdn) {
+		console.log(`A form is rendered 🎉`);
+	},
+
+	date: function(frm) {
+		console.log('Lol Date 🎉 ');
+	}
 });
--- night_badge/night_badge/doctype/patient/patient.py
+++ night_badge/night_badge/doctype/patient/patient.py
@@ -1,8 +1,52 @@
 # Copyright (c) 2023, NexTash (SMC-PVT) Ltd and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Patient(Document):
-	pass
+	def autoname(self):
+		pass
+		# self.name  = self.full_name
+	
+	def validate(self):
+		if frappe.db.exists("Patient", self.name):
+			visits = self.visits
+			
+			if len(visits):
+				date = visits[-1].date
+
+				# create a new document
+				doc = frappe.get_doc({
+					'doctype': 'Patient Activity',
+					'patient': self.name,
+					"activity": f"Visited on {date}"
+				})
+				doc.insert()
+	
+	@frappe.whitelist()
+	def dead(self):
+		doc = frappe.get_doc({
+			'doctype': 'Patient Activity',
+			'patient': self.name,
+			"activity": f"dEAD"
+		})
+		doc.insert()
+
+	# def on_update(self):
+	# 	frappe.msgprint(f"update")
+
+	# def on_update_after_submit(self):
+	# 	frappe.msgprint(f"update as")
+	
+	# def before_insert(self):
+	# 	frappe.msgprint("Insert")
+
+	# def on_submit(self):
+	# 	frappe.msgprint("submit")
+
+	# def on_cancel(self):
+	# 	frappe.msgprint("Cancel")
+
+	# def on_trash(self):
+	# 	frappe.msgprint("Deleted")
